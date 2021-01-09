8996473e951f7dca12f625a2fe65f8171194e20a
DieMusterloesung <christoph.tschechne@protonmail.com>
2021-01-09 13:52:57 +0100

    fixed all IDE issues in gameLogic

--- src/uni/ulm/sopra/gomoku/GameLogic.java
+++ src/uni/ulm/sopra/gomoku/GameLogic.java
@@ -71,10 +71,10 @@ public class GameLogic {
     }
 
 
-    private PropertyChangeSupport pcs;
+    private final PropertyChangeSupport pcs;
 
     private static final int BOARD_SIZE = 15;
-    private Board board;
+    private final Board board;
     private GameState myState = null;
     private GameState prev = null;
     private String player1Color;
@@ -195,20 +195,23 @@ public class GameLogic {
 
     public boolean makeDecision(String option){
         if(this.getGameState().equals(GameState.Swap2Question)){
-            if(option.equals("a")){
-                this.player1Color = BLACK;
-                this.player2Color = WHITE;
-                this.setGameState(GameState.AwnserA);
-            }else if(option.equals("b")){
-                this.player1Color = WHITE;
-                this.player2Color = BLACK;
-                this.setGameState(GameState.AwnserB);
-                finishTurn();
-            }else if(option.equals("c")){
-                this.setGameState(GameState.AwnserC);
-            }else{
-                System.err.println("Option not avaible");
-                return false;
+            switch (option) {
+                case "a" -> {
+                    this.player1Color = BLACK;
+                    this.player2Color = WHITE;
+                    this.setGameState(GameState.AwnserA);
+                }
+                case "b" -> {
+                    this.player1Color = WHITE;
+                    this.player2Color = BLACK;
+                    this.setGameState(GameState.AwnserB);
+                    finishTurn();
+                }
+                case "c" -> this.setGameState(GameState.AwnserC);
+                default -> {
+                    System.err.println("Option not avaible");
+                    return false;
+                }
             }
         }else if(this.getGameState().equals(GameState.ColorPick)){
             if(option.equals("a")){
@@ -288,24 +291,18 @@ public class GameLogic {
     }
 
     public String getStringRepresentation(){
-        String out = "";
+        StringBuilder out = new StringBuilder();
         for(int x = 0; x < BOARD_SIZE; x++){
             for (int y = 0; y < BOARD_SIZE; y++) {
-                switch (board.getField(x, y)){
-                    case White:
-                        out += "o";
-                        break;
-                    case Black:
-                        out += "x";
-                        break;
-                    case Empty:
-                        out += ".";
-                        break;
+                switch (board.getField(x, y)) {
+                    case White -> out.append("o");
+                    case Black -> out.append("x");
+                    case Empty -> out.append(".");
                 }
             }
-            out += "\n";
+            out.append("\n");
         }
-        return out;
+        return out.toString();
     }
 
     public String getWinner() {
